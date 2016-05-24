a240434d3e6fa5a36e7b51e614aae93782f8c9e1
marvinscharle <marvin.scharle@conclurer.com>
2016-05-24 23:08:04 +0200

    Fix for zone.js error "More tasks executed then were scheduled" in Safari, explicit call of NgZone not required in zone.js ^0.6.12

--- components/file-upload/file-item.class.ts
+++ components/file-upload/file-item.class.ts
@@ -1,5 +1,3 @@
-import {NgZone} from '@angular/core';
-
 import {FileLikeObject} from './file-like-object.class';
 import {FileUploader} from './file-uploader.class';
 
@@ -20,7 +18,6 @@ export class FileItem {
   public isError:boolean = false;
   public progress:number = 0;
   public index:number = void 0;
-  private _zone:NgZone;
 
   private uploader:FileUploader;
   private some:any;
@@ -33,7 +30,6 @@ export class FileItem {
     this.file = new FileLikeObject(some);
     this._file = some;
     this.url = uploader.options.url;
-    this._zone = new NgZone({ enableLongStackTrace: false });
   }
 
   public upload():void {
@@ -97,9 +93,7 @@ export class FileItem {
   }
 
   public _onProgress(progress:number):void {
-    this._zone.run(() => {
-      this.progress = progress;
-    });
+    this.progress = progress;
     this.onProgress(progress);
   }
 
