e437b877bd3d0a908ace11ee9befa1e6767a81f8
Dmitriy Shekhovtsov <valorkin@gmail.com>
2016-10-17 17:55:22 +0300

    Updates

--- Users/valorkin/work/open-source/ng2-file-upload/components/file-upload/file-type.class.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class FileType {
-    static mime_doc: string[];
-    static mime_xsl: string[];
-    static mime_ppt: string[];
-    static mime_psd: string[];
-    static mime_compress: string[];
-    static getMimeClass(file: any): string;
-    static fileTypeDetection(inputFilename: string): string;
-}
--- Users/valorkin/work/open-source/ng2-file-upload/demo/components/file-upload/simple-demo.d.ts
+++
@@ -1,8 +0,0 @@
-import { FileUploader } from '../../../ng2-file-upload';
-export declare class SimpleDemoComponent {
-    uploader: FileUploader;
-    hasBaseDropZoneOver: boolean;
-    hasAnotherDropZoneOver: boolean;
-    fileOverBase(e: any): void;
-    fileOverAnother(e: any): void;
-}
