820a1ced034b2d11ae7215122fe9f21d1b4cb9ca
Dmitriy Shekhovtsov <valorkin@gmail.com>
2017-01-17 16:39:11 +0200

    Updates

--- home/valorkin/work/open-source/ng2-file-upload/demo/components/file-upload-section.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class FileUploadSectionComponent {
-    name: string;
-    currentHeading: string;
-    doc: string;
-    tabs: any;
-    select(e: any): void;
-}
--- home/valorkin/work/open-source/ng2-file-upload/ng2-file-upload.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './components/file-upload/file-select.directive';
-export * from './components/file-upload/file-drop.directive';
-export * from './components/file-upload/file-uploader.class';
-export { FileUploadModule } from './components/file-upload/file-upload.module';
