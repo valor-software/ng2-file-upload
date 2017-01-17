d6e75984ed703bac4586c4c06d66f8d3fece613a
Kevin FONTAINE <kfontaine-smartwave@users.noreply.github.com>
2017-01-17 14:03:15 +0100

    #492 Add FileItem in declaration ng2-file-upload.d.ts (#523)

--- ng2-file-upload.ts
+++ ng2-file-upload.ts
@@ -1,5 +1,6 @@
 export * from  './components/file-upload/file-select.directive';
 export * from  './components/file-upload/file-drop.directive';
 export * from  './components/file-upload/file-uploader.class';
+export * from './components/file-upload/file-item.class';
 
 export { FileUploadModule } from './components/file-upload/file-upload.module';
