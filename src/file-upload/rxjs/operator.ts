5b267936620bcd7901b39c4d8cbd2b4698428b6a
Ravinder Payal <ravinderpayalspcl@gmail.com>
2017-02-04 21:23:58 +0530

    Create operator.ts
    Contains required rxjs operators' import

---
+++ src/file-upload/rxjs/operator.ts
@@ -0,0 +1,6 @@
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/toPromise';
