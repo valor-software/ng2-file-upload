98b19e7ce43757ca07e9d7b8c7081a9faf584a9f
Martin Kopeček <github@smallhill.cz>
2017-04-28 18:38:21 +0200

    Replace file in queue when single select
    I believe it wouldbe more clear when non-multiple selects would replace the file in queue.

--- src/file-upload/file-select.directive.ts
+++ src/file-upload/file-select.directive.ts
@@ -1,12 +1,15 @@
 import { Directive, ElementRef, Input, HostListener } from '@angular/core';
 
 import { FileUploader } from './file-uploader.class';
+import { FileItem } from './file-item.class';
 
 // todo: filters
 
 @Directive({selector: '[ng2FileSelect]'})
 export class FileSelectDirective {
   @Input() public uploader:FileUploader;
+
+  lastFile:FileItem;
 
   protected element:ElementRef;
 
@@ -22,7 +25,7 @@ export class FileSelectDirective {
     return void 0;
   }
 
-  public isEmptyAfterSelection():boolean {
+  public isMultiple():boolean {
     return !!this.element.nativeElement.attributes.multiple;
   }
 
@@ -34,9 +37,16 @@ export class FileSelectDirective {
     let filters = this.getFilters();
 
     // if(!this.uploader.isHTML5) this.destroy();
+    
+    // if not multiple, remove previously added file so we can replace it with the new one
+    if(!this.isMultiple() && this.lastFile & !this.lastFile.uploaded) this.uploader.removeFromQueue(this.lastFile);
 
     this.uploader.addToQueue(files, options, filters);
-    if (this.isEmptyAfterSelection()) {
+    
+    // save file so we can remove it later
+    this.lastFile = this.uploader.queue[this.uploader.queue.length - 1];
+    
+    if (this.isMultiple()) {
       // todo
       this.element.nativeElement.value = '';
       /*this.element.nativeElement
