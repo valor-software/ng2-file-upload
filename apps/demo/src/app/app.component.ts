326e3ac1a884f000f57b3fb6e30cd32f5ec4622c
SvetlanaMuravlova <83700465+SvetlanaMuravlova@users.noreply.github.com>
2021-09-03 15:12:02 +0300

    feat(version): added npm versions (#1183)
    * feat(version): added npm versions
    
    * feat(version): added npm versions

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -10,9 +10,19 @@ const gettingStarted = require('html-loader!markdown-loader!../getting-started.m
       <h1>ng2-file-upload</h1>
       <p>The Angular2 File Upload directives</p>
       <a class="btn btn-primary" href="https://github.com/valor-software/ng2-file-upload">View on GitHub</a>
-      <div class="row">
-        <div class="col-lg-1"><iframe src="https://ghbtns.com/github-btn.html?user=valor-software&repo=ng2-file-upload&type=star&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe></div>
-        <div class="col-lg-1"><iframe src="https://ghbtns.com/github-btn.html?user=valor-software&repo=ng2-file-upload&type=fork&count=true" frameborder="0" scrolling="0" width="170px" height="20px"></iframe></div>
+      <div class="row d-flex" style="padding-top: 15px">
+        <div class="col-lg-1 block_links">
+          <a href="https://npmjs.org/ng2-file-upload" style="display: flex">
+            <img src="https://img.shields.io/npm/v/ng2-file-upload/latest.svg" alt="npm latest version" >
+          </a>
+        </div>
+        <div class="col-lg-1 block_links">
+          <a href="https://npmjs.org/ng2-file-upload" style="display: flex">
+            <img src="https://img.shields.io/npm/v/ng2-file-upload/next.svg" alt="npm next version" >
+          </a>
+        </div>
+        <div class="col-lg-1 block_links"><iframe src="https://ghbtns.com/github-btn.html?user=valor-software&repo=ng2-file-upload&type=star&count=true" frameborder="0" scrolling="0" width="100px" height="20px"></iframe></div>
+        <div class="col-lg-1 block_links"><iframe src="https://ghbtns.com/github-btn.html?user=valor-software&repo=ng2-file-upload&type=fork&count=true" frameborder="0" scrolling="0" width="100px" height="20px"></iframe></div>
       </div>
     </div>
   </main>
