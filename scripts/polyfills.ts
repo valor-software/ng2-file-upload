1fe752d9f0665568dee3340c47f7e0964635ab6a
SvetlanaMuravlova <83700465+SvetlanaMuravlova@users.noreply.github.com>
2023-07-21 20:53:06 +0300

    feat: add angular 16 support(#1258)
    ---------
    
    Co-authored-by: Dmitriy Shekhovtsov valorkin <valorkin@gmail.com>

--- scripts/polyfills.ts
+++
@@ -1,5 +0,0 @@
-// This file includes polyfills needed by Angular 2 and is loaded before
-// the app. You can add your own extra polyfills to this file.
-import 'ts-helpers';
-import 'zone.js';
-import 'classlist-polyfill';
